d5a53dff8297c2386ebb6cccb5b742c2c71e72c7
Ruta Wheelock <wheel477@umn.edu>
2017-12-03 16:31:45 -0600

    Implemented simple what/who/where question answering. Started working on When questions.

--- qa-system.py
+++ qa-system.py
@@ -54,35 +54,71 @@ def main():
             continue
         
 
-        # Processing WHO and WHAT questions
-        if first_word == 'who' or first_word == 'what':
+        # Processing Queations with is/was/are/were as a second word
+        if word_list[1] in ['is', 'was', 'are', 'were']:        
 
-            # Question is of type: 'Who/what is/was somebody/something?'
-            if word_list[1] == 'is' or word_list[1] == 'was':        
+            # Retrieve search term, do not include acticles.
+            if word_list[2] in ['a', 'an', 'the']:
+                subject = ' '.join(word_list[3:])
+            else:
                 subject = ' '.join(word_list[2:])
 
-                try:
-                    page = wptools.page(subject).get_query(show=False)
-                    answer = page.data['description']
-                    reply = "=> Answer: {0} {1} {2}.".format(subject, word_list[1], answer)
-                    print reply
-                except LookupError:
-                    print "Sorry, I can't answer that."
-                    continue
 
-                try:
-                    page = wptools.page(subject).get_query(show=False)
-                    extract = page.data['extract'].split('</p>')
+            if first_word == 'when':
+                subject = ' '.join(subject.split()[:-1])
+                verb1 = word_list[1]
+                verb2 = word_list[-1]
 
-                    findAnswerRE = re.compile(r'.*(?P<answer>\b(is|are|was|were)\b \b(a|an|the)\b (\w+\s*)+)[,.].*', re.IGNORECASE)
-                    findings = findAnswerRE.match(extract[0])
+                
+            print subject
+                
+            try:
+                page = wptools.page(subject, silent=True).get_query(show=False)
+                extract = page.data['extract'].split('</p>')
+
+                for i, paragraph in enumerate(extract):
+                    # Remove non-Ascii characters
+                    extract[i] = re.sub(r'[^\x00-\x7F]+','', paragraph)
+                    # Remove html tags
+                    extract[i] = re.sub(r'<.*?>','', paragraph)
+
+                first_paragraph = extract[0]              
+                whole_extract = ' '.join(extract)
+
+                # Regular expressions
+                Who_What_RE = re.compile(r'(?P<answer>\b(is|are|was|were)\b \b(a|an|the)\b .*?(?=[,.]\s))', re.IGNORECASE)
+                Where_RE = re.compile(r'(?P<answer>\b(on|in|located)\b \b(the|a|an|in)\b .*?(?=[,.]\s))', re.IGNORECASE)
+                When_RE = re.compile(r'{} {} (?P<answer>\b(on|in)\b .*?(?=[,.]\s))'.format( verb1, verb2), re.IGNORECASE)
+
+                
+                reply = "=> Sorry, I can't aswer that."
+                
+                # Processing WHO and WHAT questions
+                if first_word == 'who' or first_word == 'what':
+                    findings = Who_What_RE.search(first_paragraph)
                     answer = findings.group('answer')
-                    reply = "=> Answer: {0} {1}.".format(subject, answer)
-                    print reply
+                    reply = "=> Answer: {0} {1}.".format(subject.title(), answer)
+
+                # Processing WHERE questions
+                elif first_word == 'where':
+                    findings = Where_RE.search(first_paragraph)
+                    answer = findings.group('answer')
+                    reply = "=> Answer: {0} {1} {2}.".format(subject.title(), word_list[1], answer)
+
+                # Processing WHEN questions
+                else:
+                    findings = When_RE.search(whole_extract)
+                    answer = findings.group('answer')                     
+                    reply = "=> Answer: {0} {1} {2} on {3}.".format(subject.title(), verb1, verb2, answer)
+
+                print reply
                     
-                except LookupError, AttributeError:
-                    print "Sorry, I can't answer that."
-                    continue
+            except (AttributeError, LookupError):
+                print "=> Sorry, I can't answer that."
+                continue
+
+
+            
 
     # Close the log file
     log_file.close()
